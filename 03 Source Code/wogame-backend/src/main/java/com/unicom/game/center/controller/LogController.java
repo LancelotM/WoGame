1e7cd4cf6ae67b285041e083b82f5bed6c242ddd
yinzhuancheng <yinzhuancheng@yeah.net>
2014-06-30 22:09:01 +0800

    update log business @Author : Alex

--- 03 Source Code/wogame-backend/src/main/java/com/unicom/game/center/controller/LogController.java
+++ 03 Source Code/wogame-backend/src/main/java/com/unicom/game/center/controller/LogController.java
@@ -2,9 +2,12 @@ package com.unicom.game.center.controller;
 
 import java.util.List;
 
-import com.unicom.game.center.model.GameInfo;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +18,10 @@ import com.unicom.game.center.business.GameTrafficBusiness;
 import com.unicom.game.center.business.LoginInfoBusiness;
 import com.unicom.game.center.business.PageTrafficBusiness;
 import com.unicom.game.center.model.GameDisplayModel;
+import com.unicom.game.center.model.GameInfo;
 import com.unicom.game.center.model.JsonParent;
+import com.unicom.game.center.utils.AESEncryptionHelper;
+import com.unicom.game.center.utils.Utility;
 
 /**
  * Created with IntelliJ IDEA.
@@ -35,21 +41,57 @@ public class LogController {
 
     @Autowired
     private GameTrafficBusiness gameTrafficService;
+    
+	@Value("#{properties['backend.secret.key']}")
+	private String backendKey;    
+    
+    @RequestMapping(value = "/log", method = {RequestMethod.GET})
+    public ModelAndView ShowLogInfo(@RequestParam(value="token",required = false) String token,
+    		@RequestParam(value="type",required=false) String type,
+    		HttpSession session){
+    	ModelMap model = new ModelMap();
+    	String channelId = null;
+    	
+    	if(!Utility.isEmpty(token)){
+    		try {
+				channelId = AESEncryptionHelper.decrypt(token, backendKey);
+				session.setAttribute("developer_channel", channelId);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    	}
+    	
+    	channelId = (String)session.getAttribute("developer_channel");
+    	
+    	if(null == channelId){
+    		Boolean isAdmin = (Boolean)session.getAttribute("admin");    		
+    		if(null == isAdmin || (null != isAdmin && !isAdmin.booleanValue())){
+    			return new ModelAndView("/index", model); 
+    		}
+    	}
+
+        model.put("channelId",(null != channelId) ? Integer.parseInt(channelId) : 0);
+        model.put("type",(null != type) ? type : "1");
+        return new ModelAndView("/log", model); 
+    }    
 
     @RequestMapping(value = "/getlog", method = {RequestMethod.POST})
-    public ModelAndView getLog(@RequestParam(value="type",required=true) String type,@RequestParam(value="channelId",required = true) int channelID){
-        ModelAndView modelView = new ModelAndView();
-        if("1".equals(type)){
-            modelView.setViewName("log");
-        }else if("2".equals(type)){
-            modelView.setViewName("monthLog");
-        }
-        modelView.addObject("channelId",channelID);
-        return modelView;
+    public ModelAndView getLog(@RequestParam(value="type",required=false) String type,
+    		@RequestParam(value="channelId",required = true) Integer channelID){
+    	ModelMap model = new ModelMap();    	
+        model.put("channelId",channelID);
+        model.put("type",(null != type) ? type : "1");
+        return new ModelAndView("/log", model); 
     }
 
     @RequestMapping(value = "/userLoginLog", method = {RequestMethod.POST})
-    public @ResponseBody JsonParent userLoginCount(@RequestParam(value="type",required=true) String type,@RequestParam(value="channelId",required = true) int channelID){
+    public @ResponseBody JsonParent userLoginCount(@RequestParam(value="type",required=false) String type,
+    		@RequestParam(value="channelId",required = true) Integer channelID){
+    	
+    	if(Utility.isEmpty(type)){
+    		type = "1";
+    	}
+    	
         int dateType = Integer.parseInt(type);
         JsonParent displayModel = null;
         if(dateType == 1){
@@ -61,7 +103,12 @@ public class LogController {
     }
 
     @RequestMapping(value = "/pageTrafficLog", method = {RequestMethod.POST})
-    public @ResponseBody JsonParent pageTrafficCount(@RequestParam(value="type",required=true) String type,@RequestParam(value="channelId",required = true) int channelID){
+    public @ResponseBody JsonParent pageTrafficCount(@RequestParam(value="type",required=false) String type,
+    		@RequestParam(value="channelId",required = true) Integer channelID){
+    	if(Utility.isEmpty(type)){
+    		type = "1";
+    	}    	
+    	
         int dateType = Integer.parseInt(type);
         JsonParent displayModel = null;
         if(dateType == 1){
@@ -73,7 +120,12 @@ public class LogController {
     }
 
     @RequestMapping(value = "/firstPageBannerLog", method = {RequestMethod.POST})
-    public @ResponseBody List<List<GameInfo>>  firstPageBannerLog(@RequestParam(value="type",required=true) String type,@RequestParam(value="channelId",required = true) int channelID){
+    public @ResponseBody List<List<GameInfo>>  firstPageBannerLog(@RequestParam(value="type",required=false) String type,
+    		@RequestParam(value="channelId",required = true) Integer channelID){
+    	if(Utility.isEmpty(type)){
+    		type = "1";
+    	}
+    	
         int dateType = Integer.parseInt(type);
         List<List<GameInfo>>  gameInfos = null;
         if(dateType == 1){
@@ -85,7 +137,12 @@ public class LogController {
     }
 
     @RequestMapping(value = "/topGameLog", method = {RequestMethod.POST})
-    public @ResponseBody List<GameDisplayModel>  topGameLog(@RequestParam(value="type",required=true) String type,@RequestParam(value="channelId",required = true) int channelID){
+    public @ResponseBody List<GameDisplayModel>  topGameLog(@RequestParam(value="type",required=false) String type,
+    		@RequestParam(value="channelId",required = true) Integer channelID){
+    	if(Utility.isEmpty(type)){
+    		type = "1";
+    	}
+    	
         int dateType = Integer.parseInt(type);
         List<GameDisplayModel>  gameInfos = null;
         if(dateType == 1){
--- 03 Source Code/wogame-backend/src/main/webapp/freemarker/menu.ftl
+++ 03 Source Code/wogame-backend/src/main/webapp/freemarker/menu.ftl
@@ -10,12 +10,14 @@
 					<div class="ul_info">
 						<a href="#" class="margin_top45" onclick="show_hidden('first_ul');"><img src="${basePath}/static/images/leftmenu_arrow.png" alt=""/>&nbsp;日志统计信息</a>
 						<ul id="first_ul" class="son_ul" style="display:none">
-							<li onmouseover="addStyle(this)" onmouseout="deleteStyle(this)" class=""><a href="${basePath}/getlog">日志总览</a></li>
+							<li onmouseover="addStyle(this)" onmouseout="deleteStyle(this)" class=""><a href="${basePath}/log">日志总览</a></li>
 							<li onmouseover="addStyle(this)" onmouseout="deleteStyle(this)" class=""><a href="${basePath}/getAllKeyWowrd">搜索日志</a></li>
 						</ul>
-					<a href="#" class="margin_top45" onclick="show_hidden('second_ul');"><img src="${basePath}/static/images/leftmenu_arrow.png" alt=""/>&nbsp;站点管理</a>
-						<ul id="second_ul" class="son_ul" style="display:none">
-							<li onmouseover="addStyle(this)" onmouseout="deleteStyle(this)" class=""><a href="${basePath}/site">建站管理</a></li>
-						</ul>
+						<#if admin?exists && admin>
+							<a href="#" class="margin_top45" onclick="show_hidden('second_ul');"><img src="${basePath}/static/images/leftmenu_arrow.png" alt=""/>&nbsp;站点管理</a>
+							<ul id="second_ul" class="son_ul" style="display:none">
+								<li onmouseover="addStyle(this)" onmouseout="deleteStyle(this)" class=""><a href="${basePath}/site">建站管理</a></li>
+							</ul>
+						</#if>
 					</div>
 				</div>
