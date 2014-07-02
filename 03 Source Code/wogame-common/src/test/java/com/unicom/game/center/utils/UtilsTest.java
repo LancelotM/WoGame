121613e2ddf22ba805518e1628b12bbb4a424680
yinzhuancheng <yinzhuancheng@yeah.net>
2014-07-02 14:48:50 +0800

    add ftp&sftp method @Author : Alex

--- 03 Source Code/wogame-common/src/test/java/com/unicom/game/center/utils/UtilsTest.java
+++ 03 Source Code/wogame-common/src/test/java/com/unicom/game/center/utils/UtilsTest.java
@@ -1,9 +1,14 @@
 package com.unicom.game.center.utils;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Date;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -20,7 +25,14 @@ public class UtilsTest {
 	@Value("#{properties['site.secret.key']}")
 	private String secretKey;
 	
+	@Autowired
+	private FTPHelper ftpHelper;	
+	
+	@Autowired
+	private SFTPHelper sftpHelper;		
+	
 	@Test
+	@Ignore
 	public void dateUtilsTest(){
 		Date today = new Date();
 		
@@ -44,6 +56,7 @@ public class UtilsTest {
 	}
 	
 	@Test
+	@Ignore
 	public void encryptionUtilsTest() throws Throwable{
 		String channelId = "18150";
 		String encyption = AESEncryptionHelper.encrypt(channelId, secretKey);
@@ -52,4 +65,60 @@ public class UtilsTest {
 		String decryption = AESEncryptionHelper.decrypt(encyption, secretKey);
 		System.out.println(decryption);
 	}
+	
+	@Test
+	@Ignore
+	public void ftpDownloadTest() throws Throwable{
+		String responseText = "";
+        InputStream response = null;
+        InputStreamReader reader = null;
+        BufferedReader breader = null;
+		
+		response = ftpHelper.downloadFile("wostore/wostorechannelapk/response/all", "wostorechannelapk_2014070200000001.tttt");
+		
+        reader = new InputStreamReader(response, "utf-8");  
+        
+        breader = new BufferedReader(reader);  
+  
+        String content = null;  
+          
+        while ((content = breader.readLine()) != null) {  
+        	responseText += content + "\n";  
+        }  
+
+		System.out.println(responseText);		
+	}
+	
+	@Test
+	public void sftpDownloadTest() throws Throwable{
+		String responseText = "";
+        InputStream response = null;
+        InputStreamReader reader = null;
+        BufferedReader breader = null;
+		
+        try{
+			response = sftpHelper.downloadFile("/wostore/wostorechannelapk/response/all/", "wostorechannelapk_2014070200000001.tttt");
+			
+	        reader = new InputStreamReader(response, "utf-8");  
+	        
+	        breader = new BufferedReader(reader);  
+	  
+	        String content = null;  
+	          
+	        while ((content = breader.readLine()) != null) {  
+	        	responseText += content + "\n";  
+	        }  
+	
+			System.out.println(responseText);
+        }catch(Exception e){
+        	throw e;
+        }finally{
+        	if(null != response)
+        		response.close();
+        	if(null != reader)
+        		reader.close();
+        	if(null != breader)
+        		breader.close();
+        }        
+	}	
 }
