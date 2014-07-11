01be6fa6a679b18ccc65c12321d6b1455da03e34
yinzhuancheng <yinzhuancheng@yeah.net>
2014-07-11 14:45:32 +0800

    update for log analyse @Author : Alex

--- 03 Source Code/wogame-common/src/main/java/com/unicom/game/center/utils/FTPHelper.java
+++ 03 Source Code/wogame-common/src/main/java/com/unicom/game/center/utils/FTPHelper.java
@@ -1,9 +1,14 @@
 package com.unicom.game.center.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -83,4 +88,68 @@ public class FTPHelper {
 		return true;
 	}
 
+    public List<String> getFileList(String path) {
+        List<String> list = new ArrayList<String>();
+   		if(connectServer()){
+   			try {
+   				ftpClient.changeWorkingDirectory(path);
+   				
+   				FTPFile[] ftpFiles = ftpClient.listFiles(path);
+   				for(int i = 0; ftpFiles != null && i < ftpFiles.length; i++){
+   					FTPFile file = ftpFiles[i];
+   					if(file.isFile()){
+   						list.add(file.getName());
+   					}
+   				}
+   				
+   				return list;
+   			} catch (Exception e){
+                   Logging.logError("Error occur in getFileList.",e);
+   			}finally {
+				try {
+					ftpClient.disconnect();
+				} catch (Exception e) {
+					Logging.logError("FTP Disconnect Error: ",e);
+				}
+   			}
+   		}
+
+   		return null;
+   	}
+    
+    public List<String> readRemoteFileByRow(String path, String filename) throws Exception {
+
+        InputStream response = null;
+        InputStreamReader reader = null;
+        BufferedReader breader = null;
+
+        List<String> list = new ArrayList<String>();
+        try {
+            String str = null;
+
+            response = downloadFile(path, filename);
+
+            reader = new InputStreamReader(response, "utf-8");
+
+            breader = new BufferedReader(reader);
+
+            while ((str = breader.readLine()) != null) {
+                list.add(str);
+            }
+
+            return list;
+        } catch (Exception e) {
+            Logging.logError("Error occur in downloadFile.", e);
+        } finally {
+            if(null != response)
+           		response.close();
+           	if(null != reader)
+           		reader.close();
+           	if(null != breader)
+           		breader.close();
+        }
+
+        return null;
+    }    
+	
 }
