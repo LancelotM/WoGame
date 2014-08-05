91c6dd0447ca71544238503f8d343d0c599e5f19
donghexin <449010393@qq.com>
2014-08-05 18:23:02 +0800

    update @Author:donghexin

--- 03 Source Code/wogame-backend/src/main/webapp/freemarker/upload.ftl
+++ 03 Source Code/wogame-backend/src/main/webapp/freemarker/upload.ftl
@@ -13,26 +13,41 @@
                     <input type='button' class='btn' value='浏览...' />
                     <input type="file" name="fileField" class="file" id="fileField" size="28" multiple="multiple" />
                 </div>
-                <div id="dialog" style="display:none;" class="min_height325">
-                    <div id="dialog_head">
-                        <span id="dialog_titile">上传文件</span>
-                    </div>
-                    <form action="" enctype="multipart/form-data" method="post" id="upload_form">
-                        <table id="upload_table">
-                            <tr class="first_tr">
-                                <td>文件名</td>
-                                <td>appid</td>
-                                <td>channelID</td>
-                                <td>操作</td>
-                                <td></td>
-                            </tr>
-                        </table>
-                    </form>
-                    <div id="footer">
-                        <a id="start_upload" href="javascript:void(0);" style="padding:2px 24px;background: url(${basePath}/static/images/state_yellow.png) no-repeat;color:#f7f7f7;font-weight: bold;">开始上传</a>
-                        <a id="cancel" href="javascript:void(0);" style="padding:2px 24px;background: url(${basePath}/static/images/state_gray.png) no-repeat;color:#f7f7f7;font-weight: bold;">取消上传</a>
-                    </div>
-                </div>
+                <form action="" enctype="multipart/form-data" method="post" id="upload_form">
+                    <table id="upload_tb" class="detail_tb" style="display: none">
+                        <thead>
+                        <tr class="table_title">
+                            <td>文件名</td>
+                            <td>appid</td>
+                            <td>channelID</td>
+                            <td>操作</td>
+                            <td></td>
+                        </tr>
+                        </thead>
+                        <tfoot>
+                        <tr>
+                            <td colspan="5" style="text-align: right">
+                                <a id="start_upload" href="javascript:void(0);" style="padding:2px 24px;background: url(${basePath}/static/images/state_yellow.png) no-repeat;color:#f7f7f7;font-weight: bold;">上传全部</a>
+                            <#--<a id="cancel_all" href="javascript:void(0);" style="padding:2px 24px;background: url(${basePath}/static/images/state_gray.png) no-repeat;color:#f7f7f7;font-weight: bold;">取消上传</a>-->
+                            </td>
+                        </tr>
+                        </tfoot>
+                        <tbody id="upload_tbody">
+
+                        </tbody>
+                    </table>
+                </form>
+
+                <#--<div id="dialog" style="display:none;" class="min_height325">-->
+                    <#--<div id="dialog_head">-->
+                        <#--<span id="dialog_titile">上传文件</span>-->
+                    <#--</div>-->
+
+                    <#--<div id="footer">-->
+                        <#--<a id="start_upload" href="javascript:void(0);" style="padding:2px 24px;background: url(${basePath}/static/images/state_yellow.png) no-repeat;color:#f7f7f7;font-weight: bold;">开始上传</a>-->
+
+                    <#--</div>-->
+                <#--</div>-->
 
             </div>
 
--- 03 Source Code/wogame-backend/src/main/webapp/static/js/upload.js
+++ 03 Source Code/wogame-backend/src/main/webapp/static/js/upload.js
@@ -3,83 +3,97 @@ $(function(){
     var files = new Array();
     var alertVal = new Array();
     var basepath = getBasePath();
-    $('#cancel').click(function(){
-        document.body.removeChild(Dialog.maskLayer);
-        $('#dialog').hide();
+    $('#cancel_all').click(function(){
+
     });
     $('#fileField').change(function(){
         $('.add_info').remove();
         var html = '';
         f = document.getElementById("fileField").files;
-        if(f.length>5){
-            alert("最多上传5个文件！");
-            return;
-        }else{
-            for(var i = 0;i<f.length;i++){
-                var name,appid,channelId,updateType;
-
-                files[i] = f[i];
-                var fileName = f[i].name;
-                if(fileName.indexOf("@") != -1){
-                    var array = fileName.split("@")
-                    name = array[1];
-                    var dataArray = array[0].split("&");
-                    appid = dataArray[0];
-                    channelId = dataArray[1];
-                    updateType = dataArray[2];
-                }else{
-                    name = fileName;
-                    appid = "";
-                    channelId = "";
-                    updateType = "";
-                }
-                html += '<tr class="add_info"><td>'+name+'</td><td><input class="upload_appid" type="text" value="'+appid+'"/></td>' +
-                    '<td><input class="channelId_upload" type="text" value="'+channelId+'" /></td>' +
-                    '<td><a href="javascript:void(0);" style="white-space: nowrap;padding:2px 24px;' +
-                    'background: url('+basepath+'/static/images/state_yellow.png) no-repeat;color:#f7f7f7;font-weight: bold;">上传文件</a></td>' +
-                    '<td><input class="upload_hid" type="hidden" value="'+i+'" /></td></tr>';
+        for(var i = 0;i<f.length;i++){
+            var name,appid,channelId,updateType;
+            files[i] = f[i];
+            var fileName = f[i].name;
+            if(fileName.indexOf("@") != -1){
+                var array = fileName.split("@")
+                name = array[1];
+                var dataArray = array[0].split("&");
+                appid = dataArray[0];
+                channelId = dataArray[1];
+                updateType = dataArray[2];
+            }else{
+                name = fileName;
+                appid = "";
+                channelId = "";
+                updateType = "";
             }
-            $('#upload_table').append(html);
-            mask('#dialog');
-            $('#upload_table a').each(function(){
-                $(this).click(function(event,type){
-                    var input = $(this).parent().siblings().children().filter("input");
-                    var appidVal = input[0].value;
-                    if(isEmpty(appidVal.trim())){
-                        alert("appid不能为空！");
-                        return;
-                    }
-
-                    var channelVal = input[1].value;
-                    if(isEmpty(channelVal.trim())){
-                        alert("channelId不能为空！");
-                        return;
-                    }else if(channelVal.trim().length != 5 && channelVal.trim().length != 8){
-                        alert("channelID必须是5字符或8字符！");
-                        return;
-                    }
-
-                    var index = input[2].value;
-                    var fileVal = files[index];
-                    alertVal[index] = sendForm(fileVal,appidVal,channelVal);
-                    if(type != 'allFile' && alertVal[index]){
-                       alert("上传成功！");
-                    }else if(type != 'allFile' && !alertVal[index]){
-                        alert("上传失败！");
-                    }
-                });
-            });
+            html += '<tr class="add_info"><td>'+name+'</td><td><input class="upload_appid" type="text" value="'+appid+'"/></td>' +
+                '<td><input class="channelId_upload" type="text" value="'+channelId+'" /></td>' +
+                '<td><a class="upload_file" href="javascript:void(0);" style="white-space: nowrap;padding:2px 24px;' +
+                'background: url('+basepath+'/static/images/state_yellow.png) no-repeat;color:#f7f7f7;font-weight: bold;">上传文件</a>' +
+                '<a class="cancel_upload" href="javascript:void(0);" style="white-space: nowrap;padding:2px 24px;' +
+                'background: url('+basepath+'/static/images/state_gray.png) no-repeat;color:#f7f7f7;font-weight: bold;">取消上传</a></td>' +
+                '<td><input class="upload_hid" type="hidden" value="'+i+'" /></td></tr>';
         }
+        $('#upload_tbody').append(html);
+        $('#upload_tb').show();
+        $('#upload_tb .upload_file').each(function(){
+            $(this).click(function(event,type){
+                var input = $(this).parent().siblings().children().filter("input");
+                var appidVal = input[0].value;
+                if(isEmpty(appidVal.trim())){
+                    alert("appid不能为空！");
+                    return;
+                }
+
+                var channelVal = input[1].value;
+                if(isEmpty(channelVal.trim())){
+                    alert("channelId不能为空！");
+                    return;
+                }else if(channelVal.trim().length != 5 && channelVal.trim().length != 8){
+                    alert("channelID必须是5字符或8字符！");
+                    return;
+                }
+
+                var index = input[2].value;
+                var fileVal = files[index];
+                alertVal[index] = sendForm(fileVal,appidVal,channelVal);
+                if(type != 'allFile' && alertVal[index]){
+                    alert("上传成功！");
+                }else if(type != 'allFile' && !alertVal[index]){
+                    alert("上传失败！");
+                }
+            });
+        });
+
+        $('#upload_tb .cancel_upload').each(function(){
+            $(this).click(function(){
+                $(this).parent().parent().remove();
+            });
+        });
     });
     $('#start_upload').click(function(){
-        $('#upload_table a').trigger('click',['allFile']);
-        alert("上传成功！");
+        $('#upload_tb .upload_file').trigger('click',['allFile']);
+        var alertStr = '';
+        for(var i = 0;i<alertVal.length;i++){
+            if(!alertVal[i]){
+                var failFile = files[i];
+                var fileName = failFile.name;
+                if(fileName.indexOf("@") != -1){
+                    var array = fileName.split("@")
+                    alertStr += array[1]+" ";
+                }else{
+                    alertStr += fileName+" ";
+                }
+            }
+        }
+        if(!isEmpty(alertStr)){
+            alert(alertStr+"上传失败！");
+        }else{
+            alert("上传成功！");
+        }
     });
 });
-
-function getDialog(){
-    mask('#dialog');
-}
 
 function sendForm(file,appid,channelid) {
     var returnVal;
