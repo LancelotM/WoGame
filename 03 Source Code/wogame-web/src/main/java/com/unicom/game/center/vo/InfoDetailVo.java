1abac12c9895ddd9130e77b51457945308e0771d
jession <sanjun.ji@gmail.com>
2014-10-18 16:08:23 +0800

    @Author:Jession 添加Get/Set

--- 03 Source Code/wogame-web/src/main/java/com/unicom/game/center/vo/InfoDetailVo.java
+++ 03 Source Code/wogame-web/src/main/java/com/unicom/game/center/vo/InfoDetailVo.java
@@ -41,4 +41,109 @@ public class InfoDetailVo {
 
     @JsonProperty("end_time")
     private long endTime;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public int getCornerMark() {
+        return cornerMark;
+    }
+
+    public void setCornerMark(int cornerMark) {
+        this.cornerMark = cornerMark;
+    }
+
+    public String getWinList() {
+        return winList;
+    }
+
+    public void setWinList(String winList) {
+        this.winList = winList;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(long createTime) {
+        this.createTime = createTime;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
 }
